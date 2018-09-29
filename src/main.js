cf403846c692c69c78b00cbeeb180b3fe0bd0d9d
wudiweigit <18818235606@163.com>
2018-09-29 14:24:24 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入重置样式
+import './assets/styles/reset.css'
+
+// 手机端 1 像素边框问题 
+import './assets/styles/border.css'
+
+//移动端 300毫秒点击延迟【在某些机型上/某些浏览器上当你使用 click事件时300毫秒然后才执行 】
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
