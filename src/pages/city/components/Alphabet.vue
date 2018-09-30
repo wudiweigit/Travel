58587dd024b87ff73f390f2a8a669e688febf0ac
wudiweigit <18818235606@163.com>
2018-09-30 18:08:16 +0800

    城市选择页-列表布局以及滚动插件【betterScroll】的使用

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">C</li>
+        <li class="item">D</li>
+        <li class="item">E</li>
+    </ul>  
+</template>
+    
+<script>
+export default{
+    name: 'CityAlphabet'
+}
+
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'   /* 在 build 的 webpack.base.conf.js配置自定义别名简化引用路径*/
+
+ 
+.list
+    display: flex
+    flex-direction: column
+    justify-content:center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+        line-height: .4rem
+        text-align: center 
+        color: $bgColor
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,133 @@
+<template>
+    <!-- 帮助我们获取 DOM 【 ref 】 -->
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <!-- 1像素边框  【 border-topbottom】 -->
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">南京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                    <!-- 1像素边框  【 border-topbottom】 -->
+                    <div class="title border-topbottom">当前城市</div>
+                    <div class="button-list">
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button">南京</div>
+                        </div>
+                    </div>
+                </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                    <div class="item border-bottom">阿克苏</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+    
+<script>
+// 引入滚动组件
+import Bscroll from 'better-scroll'
+
+
+export default{
+    name: 'CityList',
+    mounted(){
+        this.scroll = new Bscroll(this.$refs.wrapper)   //页面挂载完毕创建 滚动实例
+    }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
+@import '~styles/varibles.styl'   /* 在 build 的 webpack.base.conf.js配置自定义别名简化引用路径*/
+
+.border-topbottom    //设置.border-topbottom这个类的before/after 这个伪元素的颜色
+    &::before
+        border-color: #ccc
+    &::after
+        border-color: #ccc
+.border-bottom    //设置.border-bottom这个类的before/after 这个伪元素的颜色
+    &::before
+        border-color: #ccc
+    &::after
+        border-color: #ccc
+
+.list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    .title
+        line-height: .54rem
+        background: #eee
+        padding-left: .2rem
+        color: #666
+        font-size: .26rem
+    .button-list
+        overflow: hidden
+        padding: .1rem .6rem .1rem .1rem
+        .button-wrapper
+            float: left 
+            width: 33.33%
+            .button
+                text-align: center
+                margin: .1rem
+                border: .02rem solid #ccc
+                padding: .1rem 0
+                border-radius: .06rem
+    .item-list
+        .item
+            line-height: .76rem
+            padding-left: .2rem
+</style>
