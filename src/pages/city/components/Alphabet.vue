25bb9f8b2e06d09294415d9500b0f60fff08ddfe
wudiweigit <18818235606@163.com>
2018-10-01 20:23:12 +0800

    城市选择页-兄弟组件数据传递【city-components】

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -8,17 +8,77 @@
         @touchend="handleTouchEnd"
         >{{ key }}
         </li> -->
-        <li class="item" v-for="item of letters" :key="item" :ref="item" 
-        @touchstart="handleTouchStart" 
-        @touchmove="handleTouchMove" 
-        @touchend="handleTouchEnd" 
+
+        
+        <!-- 非父子/兄弟组件数据传递   1. 绑定个事件-->
+        <li class="item" v-for="item of letters" :key="item" :ref="item"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
         @click="handleLetterClick"
-        >{{ item }}
-        </li>
+        >{{ item }}</li>
     </ul>  
 </template>
     
 <script>
+// export default{
+//     name: 'CityAlphabet',
+//     props: {
+//         cities: Object
+//     },
+//     data(){
+//         return {
+//             touchStatus: false    // 定义标识类
+//         }
+//     },
+
+//     computed: {
+//         letters(){
+//             const letters = []      //存储字母列表 ['A', 'B', 'C']
+//             for(let i in this.cities){
+//                 letters.push(i)
+//             }
+//             return letters
+//         }
+//     },
+// // 当滑动右边字母时能让左边字母定位到指定位置
+// // 思路 ===>>获得A字母距离顶部的高度然后我滑动时看下当前手指位置距离顶部的高度我做一个差值在除以每个字母的高度
+//     methods: {
+//         handleLetterClick(e){   //定义绑定的事件方法
+//             // console.log(e.target.innerText)
+//             this.$emit('change', e.target.innerText)
+//         },
+
+        
+//         handleTouchStart(){
+//             //当手指触摸时
+//             this.touchStatus = true
+//         },
+//         // 当this.touchStatus = true 时才去出发该方法
+//         handleTouchMove(e){
+//             if(this.touchStatus){
+//                 const startY = this.$refs['A'][0].offsetTop
+//                 // console.log(startY)
+//                 // 事件对象中有个 touches 数组
+//                 const touchY = e.touches[0].clientY -79   // 79时城市选择和输入城市的高度
+//                 // console.log(touchY)
+//                 const index= Math.floor((touchY - startY) / 20)  //字母的下标 【20是字母的高度】
+
+//                 if( index >=0 && index <this.letters.length )
+//                     this.$emit('change', this.letters[index])
+//             }
+//         },
+        
+//         handleTouchEnd(){
+//             //当结束滑动时
+//             this.touchStatus = false
+//         }
+//     }
+// }
+
+
+
+
 
 export default{
     name: 'CityAlphabet',
@@ -27,55 +87,70 @@ export default{
     },
     data(){
         return {
-            touchStatus: false // 定义标识位
+            touchStatus: false,   // 定义标识类
+            startY: 0,
+            timer: null
         }
     },
 
     computed: {
-        // 根据下标找到下标对应的字母
-        letters () {
-        const letters = []  // 存储字母列表 ['A','B','C']
-            for (let i in this.cities) {
+        letters(){
+            const letters = []      //存储字母列表 ['A', 'B', 'C']
+            for(let i in this.cities){
                 letters.push(i)
             }
             return letters
         }
     },
-  // 当我滑动右端字母时能在左边自动定位到相应字母位置
-    // 思路 ==>>  获得A字母距离顶部的高度然后我滑动时看下当前手指置距离顶部的高度我做个差值在除以每个字母的高度就可以知道当前是第几个字母了
+
+
+    updated() {
+        this.startY = this.$refs['A'][0].offsetTop
+    },
+// 当滑动右边字母时能让左边字母定位到指定位置
+// 思路 ===>>获得A字母距离顶部的高度然后我滑动时看下当前手指位置距离顶部的高度我做一个差值在除以每个字母的高度
     methods: {
-        // 2.定义刚才绑定的事件
-        handleLetterClick (e){    //获取目标元素
+        handleLetterClick(e){   //定义绑定的事件方法
             // console.log(e.target.innerText)
-            this.$emit( 'change', e.target.innerText)    // 3. $emit 向外触发事件,并传值
+            this.$emit('change', e.target.innerText)
         },
 
+        
         handleTouchStart(){
-            // 当手指触摸时
+            //当手指触摸时
             this.touchStatus = true
         },
+        // 当this.touchStatus = true 时才去出发该方法
         handleTouchMove(e){
-            //只有当this.touchStatus 位true 的情况下进行处理
             if(this.touchStatus){
-                const startY = this.$refs['A'][0].offsetTop  // A 字母距离顶部的高度
-                // 事件对象中会有个 touches 的数组
-                const touchY = e.touches[0].clientY - 79     //字母距离最顶部的高度 [79是 城市选择加输入城市的高度]
-                const index = Math.floor((touchY - startY) / 20) // 手指滑动时对应字母的下标  [20是字母的高度]
-                // console.log(index)
-                
-                if( index >= 0 && index < this.letters.length ){
-                    this.$emit('change', this.letters[index])
-                }
-            }
+                // const startY = this.$refs['A'][0].offsetTop
 
+
+                // 函数节流
+                if( this.timer ){
+                    clearTimeout( this.timer )
+                }
+                this.timer = setTimeout(  ( ) => {
+                    // 事件对象中有个 touches 数组
+                    const touchY = e.touches[0].clientY -79   
+        
+                    const index= Math.floor((touchY - this.startY) / 20) 
+
+                    if( index >=0 && index <this.letters.length ){
+                        this.$emit('change', this.letters[index])
+                    }
+                }, 16)
+                
+            }
         },
+        
         handleTouchEnd(){
-            //当手指结束滑动时
+            //当结束滑动时
             this.touchStatus = false
         }
-
     }
 }
+
 
 </script>
 
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -61,6 +61,8 @@ export default{
             }
         }
     }
+
+
 }
 
 </script>
